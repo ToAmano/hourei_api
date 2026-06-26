10c5855c2ec8492a83adc45eceddba334be66df6
dirac6582 <38810216+dirac6582@users.noreply.github.com>
2026-06-26 19:39:34 +0900

    refactor: simplify __init__.py files and use direct explicit imports

--- src/elaws_parser/__init__.py
+++ src/elaws_parser/__init__.py
@@ -1,26 +1,2 @@
 # flake8: noqa
 __version__ = "0.1.0"
-
-from elaws_parser.api import (
-    extract_sections_from_xml,
-    get_lawdata_from_law_id,
-    get_lawdata_from_lawname,
-    get_lawid_from_lawtitle,
-    save_xml_string_to_file,
-)
-from elaws_parser.parser import (
-    LawToYamlConverter,
-    LawXmlParser,
-    convert_xml_to_text,
-    convert_xml_to_yaml,
-)
-
-# LLM/LangGraph機能はオプショナル依存関係のため、インストールされていない場合は無視する
-try:
-    from elaws_parser.extraction import (
-        LegalExtractionConfig,
-        YamlArticleExtractor,
-        create_legal_extraction_system,
-    )
-except ImportError:
-    pass
--- src/elaws_parser/extraction/__init__.py
+++ src/elaws_parser/extraction/__init__.py
@@ -1,13 +1,1 @@
-try:
-    from .law_extraction_v2 import (
-        LegalExtractionConfig,
-        YamlArticleExtractor,
-        create_legal_extraction_system,
-    )
-    __all__ = [
-        "LegalExtractionConfig",
-        "create_legal_extraction_system",
-        "YamlArticleExtractor",
-    ]
-except ImportError:
-    __all__ = []
+# Extraction package for elaws_parser
